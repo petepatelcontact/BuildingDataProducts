da813d6059cd447eb7090eaa95b0d3e675353ec2
ppatel <petepatelcontact@gmail.com>
2014-07-27 18:16:58 -0400

    Added Comments

--- server.R
+++ server.R
@@ -1,19 +1,28 @@
 library(shiny)
 shinyServer(
   function(input, output){
+    #
+    # Get inputs and assign to output variables to display what users entered
+    #
     output$tankSize <- renderText(input$tankSize)
     output$mpg <- renderText(input$mpg)
-    output$remaining <- renderText({calculateUsed(input$tankSize, input$used)})
+    #
+    # Calculate remaining gas in gallons and miles available and assign to output
+    # variables for display
+    #
+    output$remaining <- renderText({calculateRemaining(input$tankSize, input$used)})
     output$availDist <- renderText({calculateAvailDist(input$tankSize, input$mpg, input$used)})
   }
 )
 
-calculateUsed <- function (tankSize,  used) 
+# Function to calculate Gallons remaining
+calculateRemaining <- function (tankSize,  used) 
 {
   result <- (tankSize * (1-(used/100)))
   return(round(result, digits = 2))
 }
 
+# Function to calculate distance in miles available
 calculateAvailDist <- function (tankSize, mpg, used) 
 {
   result <- (tankSize * (1-(used/100))) * mpg
--- ui.R
+++ ui.R
@@ -2,9 +2,19 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Car Trip Calculator"),
   sidebarPanel(
+    #
+    # Get Inputs from user for Tank Size and Miles Per Gallon
+    # Set min and max values, any values out of this range will show
+    # input field in red.
+    #
     h4('Car Fuel Capacity'),
     numericInput('tankSize', 'Tank Size (Gallons 12-40)', 18,min = 12, max = 40),
     numericInput('mpg', 'Miles Per Gallon (8-50)', 23,min = 8, max = 50),
+    #
+    # Get Input from user to get percent of tank used
+    # Set min and max values, any values out of this range will show
+    # input field in red.
+    #
     h4('Usage'),
     numericInput('used', 'Tank Used (%)', 50, min = 0, max = 100),
     helpText("This application allows you to determine distance available ",
@@ -15,10 +25,16 @@ shinyUI(pageWithSidebar(
              "calculate distance car can travel on the remaining gas in tank.")
   ),
   mainPanel(
+    #
+    # Display back the inputs entered by the user
+    #
     h4('Fuel Capacity (gallons)'),
     textOutput("tankSize"),
     h4('Fuel Economy (mpg)'),
     textOutput("mpg"),
+    #
+    # Show the calculated value of remaining gas and milage
+    #
     h4('Fuel Remaining (gallons)'),
     textOutput("remaining"),
     h4('Remaining Distance (miles)'),
